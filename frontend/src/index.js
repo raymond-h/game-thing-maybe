a651c504a13d2597d9850d39bb634099debaec2c
Raymond Hammarling <raymond@hammarlings.se>
2018-12-02 19:16:49 +0100

    Add deployment of frontend to Netlify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ console.log('hello world!', document.getElementById('root'));
 
 const apiUrl = process.env.API_URL.replace(/{hostname}/g, location.hostname);
 
-fetch(apiUrl + '/test')
-  .then(res => res.text())
-  .then(text => console.log('From backend:', text))
+fetch(apiUrl + '/some-json')
+  .then(res => res.json())
+  .then(json => console.log('From backend:', json))
   .catch(err => console.error(err));
