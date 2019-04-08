72614d5c4f3a5948cacb53c30afbab81bd6f6d54
Raymond Hammarling <raymond@hammarlings.se>
2019-04-08 02:20:02 +0200

    Add way to change username

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <p>This is the main page!! {{ userInfo }}</p>
+    <p>
+      This is the main page!! {{ userInfo }}
+      <input type="text" v-model="newUsername">
+      <button
+        v-stream:click="{ subject: changeUsernameBtn$, data: newUsername }"
+        :disabled="isUsernameBeingChanged"
+      >{{ isUsernameBeingChanged ? 'Changing...' : 'Change username' }}</button>
+    </p>
     <InviteList />
   </div>
 </template>
 
 <script>
 import * as rxjs from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, pluck, startWith, last, mapTo } from 'rxjs/operators';
 
 import * as api from '../api';
 import authService from '../api/auth';
@@ -20,6 +27,14 @@ export default {
     InviteList
   },
 
+  domStreams: ['changeUsernameBtn$'],
+
+  data() {
+    return {
+      newUsername: ''
+    };
+  },
+
   subscriptions() {
     return {
       userInfo: authService.isAuthenticated$
@@ -29,6 +44,19 @@ export default {
               api.getUserInfoUpdates(channelPool, authService.userId) :
               rxjs.NEVER
           )
+        ),
+
+      isUsernameBeingChanged: this.changeUsernameBtn$
+        .pipe(
+          pluck('data'),
+          switchMap(username =>
+            rxjs.concat(
+              rxjs.of(true),
+              api.updateUserInfo({ username })
+                .pipe(last(), mapTo(false))
+            )
+          ),
+          startWith(false)
         )
     };
   }
