176421fb96d97df0b025e3775a27671cb5b21548
Raymond Hammarling <raymond@hammarlings.se>
2019-05-02 01:05:56 +0200

    Add temporary fix to only show actually-ongoing games

--- frontend/src/components/GameList.vue
+++ frontend/src/components/GameList.vue
@@ -2,12 +2,12 @@
   <div>
     <h2>Ongoing games</h2>
 
-    <p v-if="games == null">
+    <p v-if="gamesToShow == null">
       Loading...
     </p>
-    <ul v-else-if="games.length > 0">
+    <ul v-else-if="gamesToShow.length > 0">
       <li
-        v-for="game in games"
+        v-for="game in gamesToShow"
         :key="game.id"
       >
         <RouterLink :to="`/games/${game.id}`">
@@ -23,15 +23,38 @@
 
 <script>
 import * as rxjs from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 
 import * as api from '../api';
 import authService from '../api/auth';
 import { channelPool } from '../api/pusher';
 
+const isGameDone = game => game.state.playerStates[0].wonPieces === 7 || game.state.playerStates[1].wonPieces === 7;
+
+const isCurrentPlayersTurn = (game, currentUserId) => game[game.state.currentPlayer] === currentUserId;
+
 export default {
+  computed: {
+    gamesToShow() {
+      if(this.games == null) {
+        return null;
+      }
+
+      const userId = this.ownId;
+      if(userId == null) {
+        return [];
+      }
+
+      return this.games
+        .filter(game => !isGameDone(game) && isCurrentPlayersTurn(game, userId));
+    }
+  },
+
   subscriptions() {
     return {
+      ownId: authService.isAuthenticated$
+        .pipe(map(isAuth => isAuth ? authService.userId : null)),
+
       games: authService.isAuthenticated$
         .pipe(
           switchMap(isAuthed =>
