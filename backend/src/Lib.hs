01302f72e4f1453d7de301aef32765f352fc556f
Raymond Hammarling <raymond@hammarlings.se>
2019-05-02 03:06:17 +0200

    Move endpoint to be only when running dev server
    The only "leakable" information are secrets that are
    easily regenerated, and authentication was required to
    access the endpoint, meaning there was literally
    only 1 person outside of the project who could access it
    anyway, and since that person is a close friend they can be trusted.

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -209,11 +209,11 @@ createApp environment dbPool = do
     S.get "/some-json" $ auth >> do
       S.json $ object ["foo" .= Number 23, "bar" .= Number 42]
 
-    S.get "/.env" $ auth >> do
-      env <- liftIO getEnvironment
-      S.json . listOfPairsToObject $ env
-
     when isDev $
+      S.get "/.env" $ auth >> do
+        env <- liftIO getEnvironment
+        S.json . listOfPairsToObject $ env
+
       S.get "/.app-state" $ auth >> do
         users <- DB.runDbPool dbPool $ Ps.selectList [] []
         invites <- DB.runDbPool dbPool $ Ps.selectList [] []
